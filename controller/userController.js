bd7b147a19a7245928c3118e0217b80dcc69bae3
kvaisakhkrishnan <kvaisakhkrishnan@gmail.com>
2023-03-22 00:30:38 +0530

    Added Routes

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,8 @@
 const { db, transactions_db } = require('../connection');
 module.exports = {
     getEventsByDepartment : (req, res) => {
-        db.query("SELECT * FROM EventData LEFT JOIN DepartmentData ON EventData.DepartmentAbbr = DepartmentData.DepartmentAbbr order by EventData.DepartmentAbbr", (err, result) => {
+        let sql_q = "SELECT * FROM EventData LEFT JOIN DepartmentData ON EventData.DepartmentAbbr = DepartmentData.DepartmentAbbr order by EventData.DepartmentAbbr";
+        db.query(sql_q, (err, result) => {
             if(err){
                 console.log("Error in query");
                 res.send("Query Error... Contact DB Admin");
@@ -52,6 +53,43 @@ module.exports = {
             }
         )
     },
+
+
+    userLogin : (req, res) => {
+        let sql_q = `select * from UserData left join CollegeData on UserData.collegeId = CollegeData.collegeId where userEmail = '${req.params.userEmail}' and password = '${req.params.password}'`
+        db.query(sql_q, (err, result) => {
+            if(err){
+                console.log("Error in query");
+                res.send("Query Error... Contact DB Admin");
+            }
+            else
+            {
+                if(result.length == 0)
+                {
+                    res.json({
+                        status : 0
+                    });
+                }
+                else{
+                    
+                        res.json({
+                            status : 1,
+                            details : {
+                                userEmail : result[0].userEmail,
+                                fullName : result[0].fullName,
+                                activePassport : result[0].activePassport,
+                                isAmritaCBE : result[0].isAmritaCBE,
+                                collegeName : result[0].collegeName,
+                                district : result[0].district,
+                                state : result[0].state,
+                                country : result[0].country
+                            }
+                        });
+                    
+                }
+            }
+        })
+    }
 
 
 }
--- routes/userApp.js
+++ routes/userApp.js
@@ -4,5 +4,6 @@ const userController = require('../controller/userController');
 
 router.get('/events/groupByDepartment',userController.getEventsByDepartment);
 router.get('/getUser/:userEmail',userController.getUserDetails)
+router.get('/login/:userEmail/:password', userController.userLogin)
 
 module.exports = router;
