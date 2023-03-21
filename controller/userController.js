f3ab2a8505c14a4a826b866eaf1d1b825bc64372
kvaisakhkrishnan <kvaisakhkrishnan@gmail.com>
2023-03-21 23:42:58 +0530

    GetEventByDepartment

--- controller/userController.js
+++ controller/userController.js
@@ -4,9 +4,35 @@ module.exports = {
         db.query("SELECT * FROM EventData LEFT JOIN DepartmentData ON EventData.DepartmentAbbr = DepartmentData.DepartmentAbbr order by EventData.DepartmentAbbr", (err, result) => {
             if(err){
                 console.log("Error in query");
+                res.send("Query Error... Contact DB Admin");
             }
             else{
-                res.send(result);
+
+                var jsonResponse = [];
+                var eventsByDepartment = {};
+                var department = "";
+                result.forEach(eventData => {
+                    if(eventData.departmentAbbr == department)
+                    {
+                       eventsByDepartment["events"].push(eventData);
+                    }
+                    else
+                    {
+                        if(department != "")
+                        {
+                            jsonResponse.push(eventsByDepartment);
+                        }
+                        eventsByDepartment = {
+                            department : eventData.departmentName,
+                            events : [eventData]
+                        }
+                        department = eventData.departmentAbbr;
+
+                    }
+                });
+                res.send(jsonResponse);
+                
+
             }
         });
     },
@@ -18,9 +44,10 @@ module.exports = {
             sql_q,(err,result) => {
                 if(err){
                     console.log(err);
+                    res.send("Query Error... Contact DB Admin");
                 }
                 else {
-                res.send(result);
+                res.send(result[0]);
                 }
             }
         )
--- routes/userApp.js
+++ routes/userApp.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const userController = require('../controller/userController');
 
 router.get('/events/groupByDepartment',userController.getEventsByDepartment);
-
 router.get('/getUser/:userEmail',userController.getUserDetails)
 
 module.exports = router;
