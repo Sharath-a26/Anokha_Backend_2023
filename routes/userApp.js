9e0ae7b39792792596185641271223ff5cc2a125
sharath sr <76243046+Sharath-a26@users.noreply.github.com>
2023-03-24 14:52:36 +0530

    changed

--- routes/userApp.js
+++ routes/userApp.js
@@ -11,5 +11,5 @@ router.post('/verifyOTP', userController.verifyOTP);
 router.post('/insertStarrs',userController.insertStarredEvent)
 router.post('/dropStarrs',userController.dropStarredEvent)
 router.get('/getStarrs/:userEmail',controller.getUserStarrs)
-router.get('/getCrew/:teamName',controller.getCrewDetails)
+router.get('/getCrew',controller.getCrewDetails)
 module.exports = router;
