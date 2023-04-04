ce7dd6a8de1bee78340e0ab8f18a0c84cdf07a00
sharath sr <76243046+Sharath-a26@users.noreply.github.com>
2023-04-04 13:29:55 +0530

    jgh

--- controller/userWebController.js
+++ controller/userWebController.js
@@ -1,7 +1,7 @@
 const { db, transactions_db } = require('../connection');
 
-const tokenGenerator = require('../middleware/webTokenGenerator');
-const tokenValidator = require('../middleware/webTokenValidator');
+const webtokenGenerator = require('../middleware/webTokenGenerator');
+const webtokenValidator = require('../middleware/webTokenValidator');
 const otpTokenGenerator = require('../middleware/otpTokenGenerator.js');
 const otpTokenValidator = require('../middleware/otpTokenValidator');
 const randonNumberGenerator = require('../OTPGenerator/otp');
@@ -33,5 +33,68 @@ module.exports = {
         finally{
             await db_connection.release();
         }
+    },
+
+
+    userLogin : async (req,res) => {
+        console.log(req.body);
+        if(req.body.userEmail != undefined && req.body.password != undefined && !validator.isEmpty(req.body.userEmail) && !validator.isEmpty(req.body.password) && validator.isEmail(req.body.userEmail)){
+        
+            let db_connection = await db.promise().getConnection();
+            try{
+                
+                let sql_q = `select * from AnokhaCompleteUserData where userEmail = ? and password = ?`;
+                const [result] = await db_connection.query(sql_q, [req.body.userEmail, req.body.password]);
+                if(result.length == 0)
+                {
+                    res.status(404).send({error : "User not found"});
+                    return;
+                }
+                else{
+    
+                    const token = await tokenGenerator({
+                        userEmail : result[0].userEmail,
+                        fullName : result[0].fullName,
+                        collegeName : result[0].collegeName,
+                        district : result[0].district,
+                        country : result[0].country,
+                        role : "USER"
+                    });
+                    res.json({
+                        
+                            userEmail : result[0].userEmail,
+                            fullName : result[0].fullName,
+                            activePassport : result[0].activePassport,
+                            isAmritaCBE : result[0].isAmritaCBE,
+                            collegeName : result[0].collegeName,
+                            district : result[0].district,
+                            state : result[0].state,
+                            country : result[0].country,
+                            SECRET_TOKEN : token
+                        
+                    });
+                
+                }
+            }
+            catch(err)
+            {
+                const now = new Date();
+                now.setUTCHours(now.getUTCHours() + 5);
+                now.setUTCMinutes(now.getUTCMinutes() + 30);
+                const istTime = now.toISOString().slice(0, 19).replace('T', ' ');
+                fs.appendFile('ErrorLogs/errorLogs.txt', istTime+"\n", (err)=>{});
+                fs.appendFile('ErrorLogs/errorLogs.txt', err.toString()+"\n\n", (err)=>{});
+                res.status(500).send({"Error" : "Contact DB Admin if you see this message"});
+            }
+            finally 
+            {
+                db_connection.release();
+            }   
+            }
+            else
+            {
+                res.status(400).send({error : "We are one step ahead! Try harder!"});
+                return;
+            }
     }
 }
