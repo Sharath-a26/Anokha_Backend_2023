051c280d56f56195bd37d5726b6b543256664f77
sharath sr <76243046+Sharath-a26@users.noreply.github.com>
2023-04-04 13:31:07 +0530

    jhchg

--- routes/userWeb.js
+++ routes/userWeb.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const userWebController = require('../controller/userWebController');
 
 router.get('/events/all', userWebController.getAllEvents);
+router.post('/login', userWebController.userLogin);
 
 module.exports = router;
 
